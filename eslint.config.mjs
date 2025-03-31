04b7aa5fc7575dc2e00b2b33964e7e415d344285
Eladio Feijoo <eladio.feijoo@gmal.com>
2025-03-31 11:29:36 -0400

    format all code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,31 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import globals from "globals";
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import prettierPluginRecomended from "eslint-plugin-prettier/recommended";
-
+import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import prettierPluginRecomended from 'eslint-plugin-prettier/recommended'
 
 export default defineConfig([
-  globalIgnores([".turbo/**", "node_modules/**", "**/node_modules/**", "**/dist/**"]),
-  { 
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], 
+  globalIgnores([
+    '.turbo/**',
+    'node_modules/**',
+    '**/node_modules/**',
+    '**/dist/**',
+  ]),
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
   },
-  { 
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], 
-    languageOptions: { globals: {...globals.browser, ...globals.node} },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
   },
-  { 
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], 
-    plugins: { js }, 
-    extends: ["js/recommended"],
-    ignores: ["**/*.json"],
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    ignores: ['**/*.json'],
   },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  prettierPluginRecomended
-]);
+  prettierPluginRecomended,
+])
