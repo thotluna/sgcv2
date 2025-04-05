06a45d0a59f96357ffd1ebc26fcaef0dbfd6a753
Eladio Feijoo <eladio.feijoo@gmal.com>
2025-04-05 12:09:43 -0400

    Revert "Revert "feat(frontend): add client code validate""
    This reverts commit 01644eb4cfa1af7703734e8cbb59d4a017f1956e.

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,10 +1,22 @@
+import cors from 'cors'
 import express from 'express'
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.get('/', (_req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/v1/client/validate', async (req, res) => {
+  const { clientCode } = req.body
+  const data = {
+    status: 'fail',
+    message: 'El codigo de cliente no es valido',
+    code: clientCode,
+  }
+  res.send(data)
 })
 
 app.listen(3001, () => {
