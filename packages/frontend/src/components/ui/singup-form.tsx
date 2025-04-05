dd2b0cce7897bdb9f4a504c47b69a672b4b3b77d
Eladio Feijoo <eladio.feijoo@gmal.com>
2025-04-04 23:43:09 -0400

    feat(frontend): add singup form

---
+++ packages/frontend/src/components/ui/singup-form.tsx
@@ -0,0 +1,151 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { z } from 'zod'
+import Link from 'next/link'
+
+const formSchema = z
+  .object({
+    clientCode: z
+      .string()
+      .min(16, 'El codigo debe tener al menos 16 caracteres')
+      .max(16, 'El codigo no puede tener más de 16 caracteres'),
+    email: z.string().email('El email no es valido'),
+    password: z
+      .string()
+      .min(8, 'La contraseña debe tener al menos 8 caracteres')
+      .max(50, 'La contraseña no puede tener más de 50 caracteres'),
+    confirmPassword: z
+      .string()
+      .min(8, 'La contraseña debe tener al menos 8 caracteres')
+      .max(50, 'La contraseña no puede tener más de 50 caracteres'),
+  })
+  .superRefine((data, ctx) => {
+    if (data.password !== data.confirmPassword) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Las contraseñas no coinciden',
+        path: ['confirmPassword'],
+      })
+    }
+  })
+
+export function SingUpForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      clientCode: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
+  return (
+    <section className="w-full h-full flex items-center justify-center">
+      <div className="max-w-sm w-full  flex flex-col gap-1">
+        <h1 className="font-funnel text-4xl text-primary font-bold mb-8">
+          Registro
+        </h1>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 w-full"
+          >
+            <FormField
+              control={form.control}
+              name="clientCode"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Codigo de cliente</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="escribe aqui el codigo de tu empresa"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="escribe aqui tu nombre de usuario"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="xxxxxxxx" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirma el password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="xxxxxxxx" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex justify-between flex-row-reverse">
+              <Button type="submit">Submit</Button>
+              <Button
+                variant="outline"
+                type="reset"
+                onClick={() => form.reset()}
+              >
+                Reset
+              </Button>
+            </div>
+          </form>
+        </Form>
+        <div>
+          <p className="text-sm opacity-80 mt-8 text-center">
+            Si ya tienes cuenta, ingresa{' '}
+            <Link href="/" className="text-primary">
+              aquí
+            </Link>
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
