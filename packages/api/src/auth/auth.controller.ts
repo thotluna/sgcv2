9dd81307af9183141988196a71894d21144377cd
Eladio Feijoo <eladio.feijoo@gmal.com>
2025-04-06 23:17:13 -0400

    refactor(api): improve validate codeClient auth

--- packages/api/src/auth/auth.controller.ts
+++ packages/api/src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Response, Request } from 'express'
 import { AuthRepository } from './auth.repository'
+import { AuthError, DBErrorConexion } from './errors'
 
 export class AuthController {
   static validationClientCode = async (req: Request, res: Response) => {
@@ -8,7 +9,9 @@ export class AuthController {
     const repository = new AuthRepository()
 
     try {
-      if (!(await repository.validateCodeClient(clientCode))) {
+      const hasCode = await repository.validateCodeClient(clientCode)
+
+      if (!hasCode) {
         res.status(401).send({
           status: 'fail',
           message: 'codigo no valido',
@@ -19,12 +22,21 @@ export class AuthController {
         status: 'ok',
         code: clientCode,
       })
-    } catch {
-      console.log('entando')
-      res.status(401).send({
-        status: 'fail',
-        message: 'error en la conexion. por favor intentelo mas tarde',
-      })
+    } catch (error) {
+      if (error instanceof AuthError) {
+        res.status(401).send({
+          status: 'fail',
+          message: error.message,
+        })
+        return
+      }
+      if (error instanceof DBErrorConexion) {
+        res.status(401).send({
+          status: 'fail',
+          message: 'error en la conexion. por favor intentelo mas tarde',
+        })
+        return
+      }
     }
   }
 }
--- packages/api/src/auth/auth.repository.ts
+++ packages/api/src/auth/auth.repository.ts
@@ -1,19 +1,17 @@
 import { type Database } from '@sgcv2/shared'
-import {
-  createClient,
-  PostgrestError,
-  SupabaseClient,
-} from '@supabase/supabase-js'
+import { createClient, SupabaseClient } from '@supabase/supabase-js'
+import { AuthError, DBErrorConexion } from './errors'
 
 export class AuthRepository {
   private client: SupabaseClient = createClient<Database>(
     process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!,
+    process.env.SUPABASE_SERVICE_ROL!,
   )
 
   validateCodeClient = async (codeClient: string): Promise<boolean> => {
-    const beforeTime = new Date(Date.now() - 20 * 60 * 60 * 1000)
-    const { data, error } = await this.client
+    const beforeTime = new Date(Date.now() - 48 * 60 * 60 * 1000)
+
+    const { error, data } = await this.client
       .from('client-code')
       .select('created_at, claimed')
       .eq('code', codeClient)
@@ -21,16 +19,21 @@ export class AuthRepository {
       .gte('created_at', beforeTime.toISOString())
       .single()
 
-    if (data) return true
+    console.log({ error, data })
 
     if (error) {
-      if (error instanceof PostgrestError) {
-        if (error.code === 'PGRST116') {
-          return false
-        }
+      if (error.code === 'PGRST116') {
+        console.log('entrando a PGRST116')
+        throw new AuthError('Codigo de cliente no válido')
       }
-      throw new Error('error conexion')
+
+      if (error.message === 'TypeError: fetch failed') {
+        throw new DBErrorConexion(
+          'Ups... hemos tenido un problema. Por favor inténtelo más tarde',
+        )
+      }
     }
-    return false
+
+    return true
   }
 }
