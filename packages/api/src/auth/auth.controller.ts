9b948a09d2e1a0a1dc41b4ec3e0574a62d9e0f71
Eladio Feijoo <eladio.feijoo@gmal.com>
2025-04-06 20:47:18 -0400

    feat(api): improve client code validate endpoint

--- packages/api/src/auth/auth.controller.ts
+++ packages/api/src/auth/auth.controller.ts
@@ -1,14 +1,30 @@
 import { Response, Request } from 'express'
+import { AuthRepository } from './auth.repository'
 
 export class AuthController {
   static validationClientCode = async (req: Request, res: Response) => {
     const { clientCode } = req.body
-    //TODO: validar en BBDD
 
-    const data = {
-      status: 'ok',
-      code: clientCode,
+    const repository = new AuthRepository()
+
+    try {
+      if (!(await repository.validateCodeClient(clientCode))) {
+        res.status(401).send({
+          status: 'fail',
+          message: 'codigo no valido',
+        })
+        return
+      }
+      res.send({
+        status: 'ok',
+        code: clientCode,
+      })
+    } catch {
+      console.log('entando')
+      res.status(401).send({
+        status: 'fail',
+        message: 'error en la conexion. por favor intentelo mas tarde',
+      })
     }
-    res.send(data)
   }
 }
--- packages/api/src/auth/auth.repository.ts
+++ packages/api/src/auth/auth.repository.ts
@@ -1,5 +1,36 @@
+import {
+  createClient,
+  PostgrestError,
+  SupabaseClient,
+} from '@supabase/supabase-js'
+import { Database } from '../types/database.types'
+
 export class AuthRepository {
-  hasCodeClient = async (codeClient: string) => {
-    return codeClient
+  private client: SupabaseClient = createClient<Database>(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!,
+  )
+
+  validateCodeClient = async (codeClient: string): Promise<boolean> => {
+    const beforeTime = new Date(Date.now() - 20 * 60 * 60 * 1000)
+    const { data, error } = await this.client
+      .from('client-code')
+      .select('created_at, claimed')
+      .eq('code', codeClient)
+      .eq('claimed', false)
+      .gte('created_at', beforeTime.toISOString())
+      .single()
+
+    if (data) return true
+
+    if (error) {
+      if (error instanceof PostgrestError) {
+        if (error.code === 'PGRST116') {
+          return false
+        }
+      }
+      throw new Error('error conexion')
+    }
+    return false
   }
 }
