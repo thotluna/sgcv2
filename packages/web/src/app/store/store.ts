d4e1a80b5f88702a8324524519411fe4069c3d3d
Eladio Feijoo <eladio.feijoo@gmal.com>
2025-04-10 12:50:05 -0400

    feat(web): add state manager

---
+++ packages/web/src/app/store/store.ts
@@ -0,0 +1,21 @@
+import { create } from 'zustand'
+
+export interface User {
+  role: string
+  email: string
+}
+
+export interface BasicState {
+  user: User
+  setUser: (user: User) => void
+  getUser: () => User
+}
+
+export const useStoreState = create<BasicState>((set, get) => ({
+  user: {
+    role: '',
+    email: '',
+  },
+  setUser: user => set({ user }),
+  getUser: () => get().user, // Usamos get() en lugar de useStore.getState()
+}))
