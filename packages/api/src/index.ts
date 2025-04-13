6df18de870903514363d4387b272f3f5f70b7efe
Eladio Feijoo <eladio.feijoo@gmal.com>
2025-04-13 18:27:07 -0400

    chore(api): add morgan dependecy

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,9 +2,11 @@ import express from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import authRouter from './auth/auth.routes'
+import morgan from 'morgan'
 
 const app = express()
 const PORT = process.env.PORT || 3001
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.use(cookieParser())
 app.use(
   cors({
@@ -17,6 +19,10 @@ app.use(express.json())
 const routerV1 = express.Router()
 routerV1.use('/auth', authRouter)
 app.use('/v1', routerV1)
+
+app.get('/v1/clients', (req, res) => {
+  res.send('ok')
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}!`)
