5b5f49a868da6aa09a85665479ad516dacaa861e
Eladio Feijoo <eladio.feijoo@gmal.com>
2025-04-17 16:04:47 -0400

    chore(api): remove unused customer router import

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -3,7 +3,6 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import authRouter from './auth/auth.routes'
 import morgan from 'morgan'
-import { customerRouter } from './customer/customer.route'
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -11,15 +10,14 @@ app.use(morgan('combined'))
 app.use(cookieParser())
 app.use(
   cors({
-    origin: 'http://localhost:3000', // Reemplaza con el dominio de tu aplicación Next.js
-    credentials: true, // Habilita el envío de cookies
+    origin: 'http://localhost:3000',
+    credentials: true,
   }),
 )
 app.use(express.json())
 
 const routerV1 = express.Router()
 routerV1.use('/auth', authRouter)
-routerV1.use('/customers', customerRouter)
 app.use('/v1', routerV1)
 
 app.listen(PORT, () => {
