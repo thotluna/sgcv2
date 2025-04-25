70cb7ae44d716526c87610df7332cf8c50c2b5f7
Eladio Feijoo <eladio.feijoo@gmal.com>
2025-04-24 22:12:56 -0400

    feat: add language switch component

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -1,20 +1,26 @@
 import { ErrorUrl } from './_auth/components/error-url'
 import { FormSing } from '@/app/_auth/components/form-sign'
 import { DateTime } from '@/components/ui/date-time'
+import LangSwitch from '@/components/ui/lang-switch'
 import ThemeSwitch from '@/components/ui/theme-switch'
+import { useTranslations } from 'next-intl'
 import { Suspense } from 'react'
 
 export default function Home() {
+  const t = useTranslations('HomePage')
   return (
     <section className="w-screen h-screen grid grid-cols-2">
       <div className="bg-[url(/images/bg.avif)] bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center h-full text-emerald-50 font-funnel">
-        <h1 className="text-5xl ">SGCV2</h1>
+        <h1 className="text-5xl ">SGCV2 {t('title')}</h1>
         <p className="text-2xl ">Sistema de gestión y control</p>
       </div>
       <div className="flex flex-col items-center justify-center gap-4 p-8 h-full">
         <header className="w-full flex items-center justify-between flex-shrink-0">
           <DateTime />
-          <ThemeSwitch />
+          <div className="flex items-center gap-4">
+            <ThemeSwitch />
+            <LangSwitch />
+          </div>
         </header>
         <Suspense>
           <FormSing />
