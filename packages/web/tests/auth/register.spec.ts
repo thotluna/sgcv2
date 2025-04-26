3ea5d1cb6ff1ecfcf8386b1aaa9b30b7afc78fd6
Eladio Feijoo <eladio.feijoo@gmal.com>
2025-04-26 02:32:58 -0400

    refactor(auth): rename register end to end test

--- packages/web/tests/auth/register.spec.ts
+++ packages/web/tests/auth/register.spec.ts
@@ -1,117 +1,85 @@
 import { CUSTOMER_CODE } from '../mocks/handlers/auth'
-import { server } from '../mocks/server'
-import { factory } from '../utils'
-import test, { expect } from '@playwright/test'
-
-const transCommon = factory('SignPages')
-const transSignUp = factory('RegisterPage')
-const transValidation = factory('validation')
-
-test.beforeAll(() => {
-  server.listen()
-})
-
-test.afterAll(() => {
-  server.close()
-})
-
-test.beforeEach(async ({ page }) => {
-  server.resetHandlers()
-  await page.goto('http:/localhost:3000/register')
-})
-
-test('should display registration page title', async ({ page }) => {
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Create/)
-})
+import { SignUpPage } from './register.pom'
+import test from '@playwright/test'
 
 test.describe('Registration form validation errors', () => {
   test('should show JWT malformed error for badly formatted customer code', async ({
     page,
   }) => {
-    await page
-      .getByLabel(transSignUp('customer_code'))
-      .fill(CUSTOMER_CODE.CODE_NOT_FORMATTED)
-    await page.getByRole('button', { name: transCommon('submit') }).click()
-
-    await expect(page.getByText(transSignUp('jwt malformed'))).toBeVisible()
+    const signUpPage = new SignUpPage(page)
+    await signUpPage.goto()
+    await signUpPage.fillCustomerCode(CUSTOMER_CODE.CODE_NOT_FORMATTED)
+    await signUpPage.submit()
+    await signUpPage.expectErrorValidation('jwt malformed')
   })
 
   test('should show code required error when customer code is empty', async ({
     page,
   }) => {
-    await page.getByRole('button', { name: transCommon('submit') }).click()
-
-    await expect(
-      page.getByText(transValidation('customer_code_required')),
-    ).toBeVisible()
+    const signUpPage = new SignUpPage(page)
+    await signUpPage.goto()
+    await signUpPage.submit()
+    await signUpPage.expectErrorValidation('customer_code_required')
   })
 
   test('should show email invalid error for badly formatted email', async ({
     page,
   }) => {
-    await page.getByLabel(transCommon('email')).fill('test@')
-    await page.getByRole('button', { name: transCommon('submit') }).click()
-
-    await expect(page.getByText(transValidation('email_invalid'))).toBeVisible()
+    const signUpPage = new SignUpPage(page)
+    await signUpPage.goto()
+    await signUpPage.fillEmail('test@')
+    await signUpPage.submit()
+    await signUpPage.expectErrorValidation('email_invalid')
   })
 
   test('should show email required error when email is empty', async ({
     page,
   }) => {
-    await page.getByRole('button', { name: transCommon('submit') }).click()
-
-    await expect(page.getByText(transValidation('email_invalid'))).toBeVisible()
+    const signUpPage = new SignUpPage(page)
+    await signUpPage.goto()
+    await signUpPage.submit()
+    await signUpPage.expectErrorValidation('email_invalid')
   })
 
   test('should show password invalid error for short password', async ({
     page,
   }) => {
-    await page.getByLabel(transCommon('password'), { exact: true }).fill('123')
-    await page.getByLabel(transSignUp('confirm_password')).fill('123456789')
-    await page.getByRole('button', { name: transCommon('submit') }).click()
-
-    await expect(
-      page.getByText(transValidation('password_min_length')),
-    ).toBeVisible()
+    const signUpPage = new SignUpPage(page)
+    await signUpPage.goto()
+    await signUpPage.fillPassword('123')
+    await signUpPage.fillConfirmPassword('123456789')
+    await signUpPage.submit()
+    await signUpPage.expectErrorValidation('password_min_length')
   })
 
   test('should show password required error when password is empty', async ({
     page,
   }) => {
-    await page.getByLabel(transSignUp('confirm_password')).fill('123456789')
-    await page.getByRole('button', { name: transCommon('submit') }).click()
-
-    await expect(
-      page.getByText(transValidation('password_min_length')),
-    ).toBeVisible()
+    const signUpPage = new SignUpPage(page)
+    await signUpPage.goto()
+    await signUpPage.fillConfirmPassword('123456789')
+    await signUpPage.submit()
+    await signUpPage.expectErrorValidation('password_min_length')
   })
 
   test('should show confirm password invalid error for mismatched passwords', async ({
     page,
   }) => {
-    await page
-      .getByLabel(transCommon('password'), { exact: true })
-      .fill('123456789')
-    await page.getByLabel(transSignUp('confirm_password')).fill('123')
-    await page.getByRole('button', { name: transCommon('submit') }).click()
-
-    await expect(
-      page.getByText(transValidation('password_min_length')),
-    ).toBeVisible()
+    const signUpPage = new SignUpPage(page)
+    await signUpPage.goto()
+    await signUpPage.fillPassword('1234567890')
+    await signUpPage.fillConfirmPassword('123456789')
+    await signUpPage.submit()
+    await signUpPage.expectErrorValidation('password_not_match')
   })
 
   test('should show confirm password required error when confirmation is empty', async ({
     page,
   }) => {
-    await page
-      .getByLabel(transCommon('password'), { exact: true })
-      .fill('12345678')
-    await page.getByLabel(transSignUp('confirm_password')).fill('123456789')
-    await page.getByRole('button', { name: transCommon('submit') }).click()
-
-    await expect(
-      page.getByText(transValidation('password_not_match')),
-    ).toBeVisible()
+    const signUpPage = new SignUpPage(page)
+    await signUpPage.goto()
+    await signUpPage.fillPassword('123456789')
+    await signUpPage.submit()
+    await signUpPage.expectErrorValidation('password_min_length')
   })
 })
