98cf59e1cb612449cf5f524659cf1eea685a69cc
Eladio Feijoo <eladio.feijoo@gmal.com>
2025-04-27 22:46:47 -0400

    refactor: move all flow of register to sign

--- packages/web/src/app/_auth/types.ts
+++ packages/web/src/app/_auth/types.ts
@@ -10,3 +10,31 @@ export type Result = {
 }
 
 export type Provider = 'google' | 'github' | 'linkedin'
+
+export interface User {
+  id: string
+  email?: string
+  created_at: string
+  role?: string
+}
+
+export interface Session {
+  provider_token?: string | null
+  provider_refresh_token?: string | null
+  access_token: string
+  refresh_token: string
+  expires_in: number
+  expires_at?: number
+  token_type: string
+  user: User
+}
+
+export type UserResponse =
+  | {
+      user: User | null
+      session: Session | null
+    }
+  | {
+      user: null
+      session: null
+    }
