e4e8ff8ca673b51d3c3a7a96251994e23e32564e
Eladio Feijoo <eladio.feijoo@gmal.com>
2025-05-01 19:30:07 -0400

    refactor: improve code

--- packages/web/src/app/(sign)/signSubmitServerAction.ts
+++ packages/web/src/app/(sign)/signSubmitServerAction.ts
@@ -1,16 +1,42 @@
 'use server'
 
-import { Result } from '../_auth/types'
+import { Result, Session, UserResponse } from '../_auth/types'
+import { getLanguageFromCookies } from '@/lib/i18n.utils'
+import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies'
 import { cookies } from 'next/headers'
-
-const NEXT_LOCALE = 'NEXT_LOCALE'
+import { redirect } from 'next/navigation'
 
 export async function sendSing<TData>(
   data: TData,
   url: string,
-): Promise<Result> {
+): Promise<Result | void> {
   const cookieStore = await cookies()
-  const lang = cookieStore.get(NEXT_LOCALE)?.value || 'es'
+  const lang = await getLanguageFromCookies(cookieStore)
+
+  const resp = await getSeccionByCredentials(data, url, lang)
+
+  if (resp.error) return resp.error
+
+  if (!resp.data || !resp.data.session)
+    return {
+      status: 'error',
+      message: 'No se encontro session',
+    }
+
+  const { session } = resp.data
+  saveAuthCookies(session, cookieStore)
+
+  redirect('/private')
+}
+
+async function getSeccionByCredentials<TData>(
+  data: TData,
+  url: string,
+  lang: string,
+): Promise<{
+  data?: UserResponse
+  error?: { status: string; message: string }
+}> {
   try {
     const res = await fetch(url, {
       method: 'POST',
@@ -23,48 +49,57 @@ export async function sendSing<TData>(
 
     if (res.status !== 200) {
       const error = await res.json()
+
       return {
-        status: error.status,
-        message: error.message,
+        error: {
+          status: error.status,
+          message: error.message,
+        },
       }
     }
 
-    const {
-      data: { session },
-    } = await res.json()
-
-    const {
-      access_token: accessToken,
-      expires_in: expiresIn,
-      expires_at: expiresAt,
-      refresh_token: refreshToken,
-      user,
-    } = session
-
-    cookieStore.delete('access_token')
-    cookieStore.delete('refresh_token')
-
-    cookieStore.set('access_token', accessToken, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      path: '/',
-      maxAge: expiresIn * 1000,
-      expires: expiresAt,
-    })
-    cookieStore.set('refresh_token', refreshToken, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      path: '/',
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    })
-
-    cookieStore.delete('code-verify')
-
-    return { status: 'ok', message: 'ok', data: user }
-  } catch {
+    return await res.json()
+  } catch (error) {
+    const e = error as Error
     return {
-      status: 'fail',
-      message: 'error en la conexion. por favor intentelo mas tarde',
+      error: {
+        status: 'error',
+        message: e.message,
+      },
     }
   }
+}
+
+function saveAuthCookies(
+  session: Session | null,
+  cookieStore: ReadonlyRequestCookies,
+) {
+  if (!session) throw new Error('falta la session')
+
+  const {
+    access_token: accessToken,
+    expires_in: expiresIn,
+    expires_at: expiresAt,
+    refresh_token: refreshToken,
+  } = session
+
+  cookieStore.delete('access_token')
+  cookieStore.delete('refresh_token')
+
+  cookieStore.set('access_token', accessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    path: '/',
+    maxAge: expiresIn * 1000,
+    expires: expiresAt,
+  })
+
+  cookieStore.set('refresh_token', refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    path: '/',
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  })
+
+  cookieStore.delete('code-verify')
 }
---
+++ packages/web/src/lib/i18n.utils.ts
@@ -0,0 +1,11 @@
+import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies'
+import { cookies } from 'next/headers'
+
+export const NEXT_LOCALE = 'NEXT_LOCALE'
+
+export async function getLanguageFromCookies(
+  cookieStore: ReturnType<typeof cookies> | ReadonlyRequestCookies,
+): Promise<string> {
+  const cookiesValue = await cookieStore
+  return cookiesValue.get(NEXT_LOCALE)?.value || 'es'
+}
