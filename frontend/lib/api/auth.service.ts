79ebf27dc2300f13fbcc1461dce967e8388d4717
Eladio Feijoo <eladio.feijoo@gmail.com>
2025-12-12 20:44:38 +0100

    fix: repair bug on user controller

--- frontend/lib/api/auth.service.ts
+++ frontend/lib/api/auth.service.ts
@@ -13,7 +13,6 @@ export const authService = {
       password,
     };
     const response = await apiClient.post<AppResponse<LoginResponse>>(`/auth/login`, loginDto);
-    console.log(response.data);
     return response.data;
   },
   logout: async () => {
