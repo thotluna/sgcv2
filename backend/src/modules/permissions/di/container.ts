39630093e0318108885b4e8026b805a866fa0d45
Eladio Feijoo <eladio.feijoo@gmail.com>
2026-01-28 13:31:34 +0100

    fix(backend): correct PermissionsService bindings in DI container

--- backend/src/modules/permissions/di/container.ts
+++ backend/src/modules/permissions/di/container.ts
@@ -1,3 +1,5 @@
+import { GetPermissions } from '@permissions/domain/get-permissions.service';
+import { ListPermissionsService } from '@permissions/domain/list-permissions.service';
 import { PermissionRepository } from '@permissions/domain/permission.repository';
 import { PermissionsService } from '@permissions/domain/permissions.service';
 import { PermissionsPrismaRepository } from '@permissions/infrastructure/persist/permissions-prisma.repository';
@@ -11,8 +13,15 @@ export const permissionsContainerModule = new ContainerModule(
       .bind<PermissionRepository>(TYPES.PermissionRepository)
       .to(PermissionsPrismaRepository)
       .inSingletonScope();
+
+    // Bind the same service to multiple interfaces if needed
     options
-      .bind<PermissionsService>(TYPES.PermissionsService)
+      .bind<ListPermissionsService>(TYPES.ListPermissionsService)
+      .to(PermissionsService)
+      .inSingletonScope();
+
+    options
+      .bind<GetPermissions>(TYPES.GetPermissionsService)
       .to(PermissionsService)
       .inSingletonScope();
   }
